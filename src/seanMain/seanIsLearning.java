246875ba43ce437033e0f2621199afcfb869446c
RoKuluro96 <58828052+RoKuluro96@users.noreply.github.com>
2019-12-15 21:30:37 -0500

    commit everyday

--- src/seanMain/seanIsLearning.java
+++ src/seanMain/seanIsLearning.java
@@ -8,15 +8,15 @@ import seanComponent.SeanDrawables;
 
 public class seanIsLearning {
 	public static void main(String[]args) {
-		Display sean = new Display();
+		//Display sean = new Display();
 		Display2 lynn = new Display2();
 		//System.out.println(sean.sip.readLine());
 
 		String lineatm =" ";
 
-		while(sean.sip.input.hasNext()) {
+		/*while(sean.sip.input.hasNext()) {
 			lineatm = sean.sip.readLine();
-		}
+		}*/
 
 		//System.out.println(sean.sip.readLine());
 
